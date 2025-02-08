5b1edb31ae007dbe774b8e63a69e93f1cda11f86
Vassili <vassili.regnier@etu.univ-amu.fr>
2025-02-08 01:14:50 +0100

    Ajout système Formspree

--- js/script.js
+++ js/script.js
@@ -18,12 +18,39 @@ carousel.style.transform = `translateX(${-index * 100}vw)`;
 
 
 
-// Redéfinit les vh (view height) pour corriger des bugs d'affichage sur mobile
-function setScreenHeight() {
-  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-}
-setScreenHeight();
-window.addEventListener('resize', setScreenHeight);
+// Formulaire de contact 
+  var form = document.getElementById("contact-form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("contact-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Envoyé !";
+        // status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
+
 
 // Menu
 const toggle_n = document.getElementById('menu-toggle');
